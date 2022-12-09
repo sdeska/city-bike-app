a4f2574a8b6585738f04c8312e5aae2730bd8952
sdeska <77407605+sdeska@users.noreply.github.com>
2022-12-09 22:24:48 +0200

    Initial commit

---
+++ src/main/java/fi/sdeska/citybike/CitybikeApplication.java
@@ -0,0 +1,13 @@
+package fi.sdeska.citybike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitybikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitybikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/sdeska/citybike/CitybikeApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.sdeska.citybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitybikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
