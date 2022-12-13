8f20a34d26a31b5edf9b27e04d1b60803832b688
sdeska <77407605+sdeska@users.noreply.github.com>
2022-12-13 21:44:40 +0200

    Add repository interfaces

---
+++ src/main/java/fi/sdeska/citybike/JourneyRepository.java
@@ -0,0 +1,9 @@
+package fi.sdeska.citybike;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JourneyRepository extends CrudRepository<Journey, Long> {
+    
+}
---
+++ src/main/java/fi/sdeska/citybike/StationRepository.java
@@ -0,0 +1,9 @@
+package fi.sdeska.citybike;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StationRepository extends CrudRepository<Station, Long> {
+    
+}
