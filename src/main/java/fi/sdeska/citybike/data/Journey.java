2c0ea1ab3527f458363a5581f238b1cf06429487
sdeska <77407605+sdeska@users.noreply.github.com>
2023-01-04 17:08:13 +0200

    Replace returning 'this' from setters with @Builder annotation

--- src/main/java/fi/sdeska/citybike/data/Journey.java
+++ src/main/java/fi/sdeska/citybike/data/Journey.java
@@ -10,9 +10,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+@Builder
+
 @Entity
 @Table(name = "journeys", schema = "public")
 public class Journey {
@@ -36,50 +41,5 @@ public class Journey {
 
     private int distance;
     private int duration;
-
-    public Journey setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public Journey setDepartureDate(DateTime departureDate) {
-        this.departureDate = departureDate;
-        return this;
-    }
-
-    public Journey setReturnDate(DateTime returnDate) {
-        this.returnDate = returnDate;
-        return this;
-    }
-
-    public Journey setDepartureStationID(int departureStationID) {
-        this.departureStationID = departureStationID;
-        return this;
-    }
-
-    public Journey setDepartureStationName(String departureStationName) {
-        this.departureStationName = departureStationName;
-        return this;
-    }
-
-    public Journey setReturnStationID(int returnStationID) {
-        this.returnStationID = returnStationID;
-        return this;
-    }
-
-    public Journey setReturnStationName(String returnStationName) {
-        this.returnStationName = returnStationName;
-        return this;
-    }
-
-    public Journey setDistance(int distance) {
-        this.distance = distance;
-        return this;
-    }
-
-    public Journey setDuration(int duration) {
-        this.duration = duration;
-        return this;
-    }
     
 }
--- src/main/java/fi/sdeska/citybike/data/Station.java
+++ src/main/java/fi/sdeska/citybike/data/Station.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
+@Builder
+
 @Entity
 @Table(name = "stations", schema = "public")
 public class Station {
@@ -28,70 +33,5 @@ public class Station {
     private int capacity;
     private double x;
     private double y;
-
-    public Station setFID(int fID) {
-        this.fId = fID;
-        return this;
-    }
-
-    public Station setID(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public Station setNameFin(String nameFin) {
-        this.nameFin = nameFin;
-        return this;
-    }
-
-    public Station setNameSwe(String nameSwe) {
-        this.nameSwe = nameSwe;
-        return this;
-    }
-
-    public Station setNameEng(String nameEng) {
-        this.nameEng = nameEng;
-        return this;
-    }
-
-    public Station setAddressFin(String addressFin) {
-        this.addressFin = addressFin;
-        return this;
-    }
-
-    public Station setAddressSwe(String addressSwe) {
-        this.addressSwe = addressSwe;
-        return this;
-    }
-
-    public Station setCityFin(String cityFin) {
-        this.cityFin = cityFin;
-        return this;
-    }
-
-    public Station setCitySwe(String citySwe) {
-        this.citySwe = citySwe;
-        return this;
-    }
-
-    public Station setOperator(String operator) {
-        this.operator = operator;
-        return this;
-    }
-
-    public Station setCapacity(int capacity) {
-        this.capacity = capacity;
-        return this;
-    }
-
-    public Station setX(double x) {
-        this.x = x;
-        return this;
-    }
-
-    public Station setY(double y) {
-        this.y = y;
-        return this;
-    }
 
 }
