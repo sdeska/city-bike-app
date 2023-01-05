e4bdf57035580b313aeb572b4351f8350f4b4463
sdeska <77407605+sdeska@users.noreply.github.com>
2023-01-05 18:58:02 +0200

    Remove checking for existing journey when saving

--- src/main/java/fi/sdeska/citybike/service/DataService.java
+++ src/main/java/fi/sdeska/citybike/service/DataService.java
@@ -16,24 +16,9 @@ import fi.sdeska.citybike.data.StationRepository;
 public class DataService {
 
     @Autowired
-    private JourneyRepository journeys;
-    @Autowired
     private StationRepository stations;
-
-    /**
-     * Saves the provided journey data into a new database entry. 
-     * Skips data where distance or duration is less than 10.
-     * @param journeyData data of the journey as a comma separated string.
-     */
-    public Journey saveJourney(Journey journey) {
-
-        Optional<Journey> savedJourney = journeys.findById((long) journey.getId());
-        if (savedJourney.isPresent()) {
-            throw new ResourceAlreadyExistsException("Journey with the given ID already exists");
-        }
-        return journeys.save(journey);
-
-    }
+    @Autowired
+    private JourneyRepository journeys;
 
     public Station saveStation(Station station) {
 
@@ -42,6 +27,17 @@ public class DataService {
             throw new ResourceAlreadyExistsException("Station with the given ID already exists");
         }
         return stations.save(station);
+
+    }
+
+    /**
+     * Saves the provided journey data into a new database entry. 
+     * Skips data where distance or duration is less than 10.
+     * @param journeyData data of the journey as a comma separated string.
+     */
+    public Journey saveJourney(Journey journey) {
+
+        return journeys.save(journey);
 
     }
     
