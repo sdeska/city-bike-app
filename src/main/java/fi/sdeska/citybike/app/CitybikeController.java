36131792cb6fd56572e843767b5c7878edcec765
sdeska <77407605+sdeska@users.noreply.github.com>
2023-01-07 20:32:28 +0200

    Implement get methods for all stations and journeys

--- src/main/java/fi/sdeska/citybike/app/CitybikeController.java
+++ src/main/java/fi/sdeska/citybike/app/CitybikeController.java
@@ -1,9 +1,16 @@
 package fi.sdeska.citybike.app;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fi.sdeska.citybike.data.Journey;
+import fi.sdeska.citybike.data.Station;
 import fi.sdeska.citybike.service.JourneyService;
 import fi.sdeska.citybike.service.StationService;
 
@@ -19,6 +26,16 @@ public class CitybikeController {
     @RequestMapping("/")
     public String home() {
         return "No place like home";
+    }
+
+    @GetMapping("/stations")
+    public ResponseEntity<List<Station>> getAllStations() {
+        return new ResponseEntity<>(stationService.fetchAllStations(), HttpStatus.OK);
+    }
+
+    @GetMapping("/journeys")
+    public ResponseEntity<List<Journey>> getAllJourneys() {
+        return new ResponseEntity<>(journeyService.fetchAllJourneys(), HttpStatus.OK);
     }
 
 }
