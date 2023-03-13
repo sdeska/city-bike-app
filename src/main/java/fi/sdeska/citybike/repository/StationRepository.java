8e591cf9a649abdbf55d720ddb526abd2b6c6257
Esa Karjalainen <esa.karjalainen@proton.me>
2023-03-13 22:54:20 +0200

    Add stuff for searching Stations WIP
    Some weird dependency version problem I've been trying to fix for hours now (╯°□°）╯︵ ┻━┻

--- src/main/java/fi/sdeska/citybike/repository/StationRepository.java
+++ src/main/java/fi/sdeska/citybike/repository/StationRepository.java
@@ -1,6 +1,5 @@
 package fi.sdeska.citybike.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import fi.sdeska.citybike.entity.Station;
@@ -9,6 +8,6 @@ import fi.sdeska.citybike.entity.Station;
  * This interface encapsulates the storage, retrieval and search functions concerning stations and the database.
  */
 @Repository
-public interface StationRepository extends JpaRepository<Station, Long>, CustomStationRepository {
+public interface StationRepository extends StationSearchRepository<Station, Long>, CustomStationRepository {
 
 }
