32c8150161a9d480151aafa8f0907f3b9d943885
Esa Karjalainen <esa.karjalainen@proton.me>
2023-03-14 22:58:40 +0200

    Fix dependency problem

--- src/main/java/fi/sdeska/citybike/CitybikeApplication.java
+++ src/main/java/fi/sdeska/citybike/CitybikeApplication.java
@@ -2,10 +2,12 @@ package fi.sdeska.citybike;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * This class is the top layer of the application. Spring launches the application through this class.
  */
+@EnableJpaRepositories("fi.sdeska.citybike.repository")
 @SpringBootApplication
 public class CitybikeApplication {
 
